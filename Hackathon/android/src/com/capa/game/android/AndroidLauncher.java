b318921fb20c24aaaa9fc8226a74bfa98b07db31
Prudhviraj Naidu <prudhvinaidu@gmail.com>
2016-10-08 14:58:20 -0400

    Add files via upload
    Initial Libgdx Game build

---
+++ Hackathon/android/src/com/capa/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.capa.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.capa.game.ProjectThisGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectThisGame(), config);
+	}
+}
---
+++ Hackathon/desktop/src/com/capa/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.capa.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.capa.game.ProjectThisGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectThisGame(), config);
+	}
+}
